5f69478a40e59e6deecc27c8d1ee27b9075884ad
Azizbek599 <azizbekabdukadirov599@gmail.com>
2026-07-07 17:10:09 +1000

    Add: 12) Uchta son: kamida bittasi boshqasidan farq qiladimi? [Azizbek599]

---
+++ lms_kod_topshiriqlari/01-Asoslar/11-Solishtirish_operatorlari__________________/12-12__Uchta_son__kamida_bittasi_boshqasidan_farq_qil.py
@@ -0,0 +1,5 @@
+a, b, c = map(int, input().split())
+print(not (a==b and (b==c)))
+# a, b, c (bitta qatorda)
+# Uchalasining hammasi teng bo'lmasa True, aks holda False.
+# (a==b and b==c) bo'lmasa True
