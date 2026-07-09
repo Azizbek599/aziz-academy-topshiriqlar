eeae47e79f19517de3c36a35910265f7496befdf
Azizbek599 <azizbekabdukadirov599@gmail.com>
2026-07-09 17:15:32 +1000

    Add: Soniyalarni soat, minut, soniyaga ajratish [Azizbek599]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-Soniyalarni_soat__minut__soniyaga_ajratish.py
@@ -0,0 +1,8 @@
+total_seconds = int(input())
+hours = total_seconds // 3600
+remaining_seconds = total_seconds % 3600
+minutes = remaining_seconds // 60
+seconds = remaining_seconds % 60
+print(hours)
+print(minutes)
+print(seconds)
